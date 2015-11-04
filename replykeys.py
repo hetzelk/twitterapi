e83e8fe45cf1a4a336eb9ea360b64b8d56b6663d
keithhetzel <keithh.8112@yahoo.com>
2015-11-04 11:23:50 -0600

    I added the same files as daves original to our repository. I changed the replykeys.py so that we havethe correct keys.

---
+++ replykeys.py
@@ -0,0 +1,10 @@
+
+#visit http://dev.twitter.com to create an application and get your keys
+keys = dict(
+consumer_key =          'vI2lbUVawolJ6StwqFNxg4c8m',
+consumer_secret =       '8ByA0rxlKs6xZPlaIxEV0T7gfUZEJKlir83fXXyNADYEOfN8b', 
+access_token =          '4125236000-IRjgSFJnwWuZstCbStIY0KCnFdZibMVTyc2VEWC',
+access_token_secret =   '82pF1LeGmzmMB1FITVzFpe8Ouu06aunkMTBGvyoXH2oQI',
+ 
+ 
+)

fd28d08d171e419491c31f92caaa78ca7797f18b
keithhetzel <keithh.8112@yahoo.com>
2015-11-06 15:58:45 -0600

    added retweet, and favorite to the existing sugarbot

--- sugarbot/SugarBot.py
+++ sugarbot/SugarBot.py
@@ -1,4 +1,4 @@
-import tweepy
+import tweepy, os, random, time
 from keys import keys
 class TwitterBot(object):
 
@@ -14,10 +14,9 @@ class TwitterBot(object):
         auth.secure = True
         auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         self.api = tweepy.API(auth)
-
-        
+        self.tweetlist = []
+        self.favlist = []     
      
-
     def tweet(self):
 
         filename=open(self.argfile,'r')
@@ -33,8 +32,60 @@ class TwitterBot(object):
             follower.follow()
             print(follower.screen_name)
 
-twit = TwitterBot()
-twit.tweet()
+
             
-			
+    def retweet(self):
+        #retweetsearchQuery = str(input("Enter searchterm for retweet. "))
+        for result in self.api.search(q="bald", lang="en"):
+            self.tweetlist.append(result.id)
+        try:
+            numTweets = len(self.tweetlist)
+            rand = random.randint(0, numTweets)
+            self.api.retweet(self.tweetlist[rand])
+            
+        except Exception as e:
+            print(e)
+            twit.retweet()
+
+        
+
+
+    def favorite(self):
+        #favsearchQuery = str(input("Enter searchterm for favorite. "))
+        for result in self.api.search(q="toupee", lang="en"):
+            self.tweetlist.append(result.id)
+        try:
+            numTweets = len(self.tweetlist)
+            rand = random.randint(0, numTweets)
+            self.api.create_favorite(self.tweetlist[rand])
+
+        except Exception as e:
+            print(e)
+            twit.favorite()
+
+
+    def main(self):
+
+        os.system("cls")
+        twit.follow()
+        counter = 0
+
+        limit = int(input("How many times do you want to repeat? ")) 
+
+        while counter <= (limit - 1):
+            
+            counter += 1
+            print("")
+            print("You just followed, favorited, tweeted, or retweeted,", counter, "time/s.")
+            twit = TwitterBot()
+            #twit.tweet()
+            twit.favorite()
+            twit.retweet()
+
+            num = random.randint(120, 240)
+            time.sleep(num)
+
+
+twit = TwitterBot()
+twit.main()			
 	
